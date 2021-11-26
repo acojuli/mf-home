a8e85e886c34de5716ea654a1c49aa1e3637415a
Julian Gomez <acojuli@gmail.com>
2021-11-25 22:08:43 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import "./index.scss";
+
+import Header from './Header';
+import Footer from './Footer';
+
+const App = () => (
+  <div className="mx-auto max-w-6xl">
+    <Header />
+    <div className="my-10">Home Content</div>
+    <Footer />
+  </div>
+);
+
+ReactDOM.render(<App />, document.getElementById("app"));
